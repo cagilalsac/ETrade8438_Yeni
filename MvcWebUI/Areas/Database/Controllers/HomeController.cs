b561836e01520f9105e58954d6a1ca73b84295b9
Cagil ALSAC <cagil.alsac@bilgeadam.com>
2022-12-03 13:51:15 +0300

    v1

--- MvcWebUI/Areas/Database/Controllers/HomeController.cs
+++ MvcWebUI/Areas/Database/Controllers/HomeController.cs
@@ -17,21 +17,75 @@ namespace MvcWebUI.Areas.Database.Controllers
 
         public IActionResult Index()
         {
+            var products = _db.Products.ToList();
+            _db.Products.RemoveRange(products);
+
+            var categories = _db.Categories.ToList();
+            _db.Categories.RemoveRange(categories);
+
             _db.Categories.Add(new Category()
             {
-                 Name = "Computer",
-                 Description = "Computer Types",
-                 Products = new List<Product>()
-                 {
-                     new Product()
-                     {
-                         Name = "Laptop",
-                         UnitPrice = 3000.5,
-                         StockAmount = 10,
-                         ExpirationDate = DateTime.Parse("03.12.2022", new CultureInfo("tr-TR"))
-                     }
+                Name = "Computer",
+                Description = "Laptops, desktops and computer peripherals",
+                Products = new List<Product>()
+                {
+                    new Product()
+                    {
+                        Name = "Laptop",
+                        UnitPrice = 3000.5,
+                        ExpirationDate = new DateTime(2032, 1, 27),
+                        StockAmount = 10
+                    },
+                    new Product()
+                    {
+                        Name = "Mouse",
+                        UnitPrice = 20.5,
+                        StockAmount = 50,
+                        Description = "Computer peripheral"
+                    },
+                    new Product()
+                    {
+                        Name = "Keyboard",
+                        UnitPrice = 40,
+                        StockAmount = 45,
+                        Description = "Computer peripheral"
+                    },
+                    new Product()
+                    {
+                        Name = "Monitor",
+                        UnitPrice = 2500,
+                        ExpirationDate = DateTime.Parse("05/19/2027", new CultureInfo("en-US")),
+                        StockAmount = 20,
+                        Description = "Computer peripheral"
+                    }
+                }
+            });
 
-                 }
+            _db.Categories.Add(new Category()
+            {
+                Name = "Home Theater System",
+                Products = new List<Product>()
+                {
+                    new Product()
+                    {
+                        Name = "Speaker",
+                        UnitPrice = 2500,
+                        StockAmount = 70
+                    },
+                    new Product()
+                    {
+                        Name = "Receiver",
+                        UnitPrice = 5000,
+                        StockAmount = 30,
+                        Description = "Home theater system component"
+                    },
+                    new Product()
+                    {
+                        Name = "Equalizer",
+                        UnitPrice = 1000,
+                        StockAmount = 40
+                    }
+                }
             });
             _db.SaveChanges();
             return Content("<label style=\"color:red\";><b>Database seed successful.</b></label>");
